e7679a836978867df5541cec5f999e4a98f564be
Peter Dz <piotreq_46@wp.pl>
2022-10-28 17:02:39 +0200

    Server z baza

---
+++ API/Data/CourierDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using API.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace API.Data
+{
+    public class CourierDbContext : DbContext
+    {
+        protected readonly IConfiguration configuration;
+
+        public CourierDbContext(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(configuration.GetConnectionString("SqlConnection"));
+        }
+        public DbSet<User> AppUsers { get; set; }
+    }
+
+    
+}
---
+++ API/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Surname { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public Role Role { get; set; }
+    }
+    public enum Role
+    {
+        Admin,Courier,User
+    }
+}
