3cd63bf3d0fec07b982edbb18e50d0f6b5ac5a68
spychuxD <dawid.spychalski00@gmail.com>
2022-12-21 15:53:38 +0100

    gb

--- AplikacjaKordynatora/Models/User.cs
+++ AplikacjaKordynatora/Models/User.cs
@@ -7,9 +7,9 @@ namespace AplikacjaKordynatora.Models
     public class User
     {
         public int id { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Proszę podać imie")]
         public string name { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Proszę podać nazwisko")]
         public string surname { get; set; }
         public int? loginCredentialsId { get; set; }
         public loginCredentials loginCredentials { get; set; }
@@ -19,7 +19,7 @@ namespace AplikacjaKordynatora.Models
         public List<Package> senderPackages { get; set; }
         public List<Package> receiverPackages { get; set; }
         public List<Order> orders { get; set; }
-		[Required]
+		[Required(ErrorMessage = "Proszę podać numer telefonu"), RegularExpression("^[0-9]{3}-[0-9]{3}-[0-9]{3}$", ErrorMessage = "Poprawny format numeru telefonu to 'xxx-xxx-xxx'")]
 		public string phoneNumber { get; set; }
     }
 }
--- AplikacjaKordynatora/Models/loginCredentials.cs
+++ AplikacjaKordynatora/Models/loginCredentials.cs
@@ -8,12 +8,12 @@ namespace AplikacjaKordynatora.Models
     {
         
         public int id { get; set; }
-        [Required,EmailAddress]
+        [Required(ErrorMessage = "Proszę podać adres e-mail"), EmailAddress]
         public string email { get; set; }
-		[Required]
+		[Required(ErrorMessage = "Proszę podać login")]
 		public string login { get; set; }
-		[Required]
-		public string password { get; set; }
+		[Required(ErrorMessage = "Proszę podać haslo"), RegularExpression(@"^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!*@#$%^&+=]).*$", ErrorMessage = "Hasło powinno zawierać małą, dużą litere, znak specjalny, cyfre i minimum 8 zanków")]
+        public string password { get; set; }
         public User user { get; set; }
     }
 }
